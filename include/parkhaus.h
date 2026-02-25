7fa91044f7116ea79655fe187c8e9b3c7ba62b44
tim-brauk <timbraukmann@gmx.de>
2026-02-25 21:39:10 +0100

    Header Datei mit Structs und Funktionsprototyp

---
+++ include/parkhaus.h
@@ -0,0 +1,27 @@
+#ifndef PARKHAUS_H
+#define PARKHAUS_H
+#include "auto.h"
+typedef struct Parkplatz
+{
+    int belegt;
+    Auto *p_auto;
+} Parkplatz;
+typedef struct Parkhaus
+{
+    int maximale_parkdauer;
+    Parkplatz *p_parkplaetze;
+    Auto *p_erstes_auto_in_der_warteschlange;
+    int anzahl_parkplaetze;
+    unsigned int belegte_parkplaetze; //belegte_parkplaetze !< parkplaetze
+} Parkhaus;
+/**
+* @brief Initialisiert das Parkhaus und reserviert den Speicher dafür.
+*
+* Reserviert Speichert für das Parkhaus und setzt p_erstes_auto_in_der_warteschlange auf NULL.
+* @param[in]  p_parkplaetze Zeiger auf das Array der Parkplaetze
+* @param[in]  anzahl_parkplaetze Anzahl der Parkplaetze
+* @param[in]  maximale_parkdauer in Stunden 
+* @return p_parkhaus Zeiger auf das Parkhaus
+*/
+Parkhaus *init_parkhaus(Parkplatz *p_parkplaetze, int anzahl_parkplaetze, int maximale_parkdauer);
+#endif /* PARKHAUS_H */
