896af32780caa33a2909bcdf3f45f999b60ea11a
Tim <timbraukmann@gmx.de>
2026-02-27 13:43:41 +0100

    recommit da vorher nicht gespeichert

---
+++ include/parkvorgang.h
@@ -0,0 +1,38 @@
+#ifndef PARKVORGANG_H
+#define PARKVORGANG_H
+#include "parkhaus.h"
+
+
+/**
+ * @brief Parkt ein Kfz auf einem zufaelligen freien Parkplatz ein.
+ *
+ * Sucht einen zufaelligen freien Parkplatz und weist dem Kfz
+ * diesen Platz zu. Es wird außerdem gespeichert an wie vielen Parkplätzen es bis dahin vorbeigekommen ist.
+ *
+ * @param[in] p_parkhaus Zeiger auf das Parkhaus
+ * @param[in] p_kfz  Zeiger auf das einzuparkende Kfz
+ * @return void
+ */
+void add_car(Parkhaus *p_parkhaus, Kfz *p_kfz);
+
+/**
+ * @brief Entfernt ein Kfz von seinem Parkplatz.
+ *
+ *Entfernt das Kfz und gibt den Speicher frei. Der parkplatz wird als frei markiert.
+ * 
+ * @param[in] p_parkhaus Zeiger auf das Parkhaus
+ * @param[in] p_kfz  Zeiger auf das zu entfernende Kfz
+ * @return void
+ */
+void remove_car(Parkhaus *p_parkhaus, Kfz *p_kfz);
+
+/**
+ * @brief Entfernt Kfz mit die die maximale Parkdauer überschritten haben.
+ *
+ * Überprüft alle geparkten kfz und entfernt die die die maximale Parkdauer überschritten haben (remove_car).
+ * 
+ * @param[in] p_parkhaus Zeiger auf das Parkhaus
+ * @return void
+ */
+void remove_cars_max_parktime(Parkhaus *p_parkhaus);
+#endif /* PARKVORGANG_H */
