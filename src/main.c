b44b9b0980ab393651226fce11c8198fc0ee071e
tim-brauk <timbraukmann@gmx.de>
2026-03-02 14:59:00 +0100

    vergessen speichern zu drücken

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "kfz.h"
+#include "parkhaus.h"
+#include "parkvorgang.h"    
+#include "simulation.h"
+#include "warteschlange.h"
+
+int main(void)
+{
+    //variablen Deklaration
+    Ganzzahl id = 0
+    Ganzzahl zeitpunkt = 0
+    Ganzzahl anzahl_parkplaetze = 0
+    Ganzzahl maximale_parkdauer = 0
+    float simulations_dauer = 0
+    Ganzzahl seed = 0
+    float wahscheinlichkeit_neues_kfz = 0
+
+    //Parameter über Terminal eingeben
+    Eingabe: anzahl_parkplaetze
+    Eingabe: maximale_parkdauer
+    Eingabe: simuations_dauer
+    Eingabe: seed
+    Eingabe: wahscheinlichkeit_neues_kfz
+}
