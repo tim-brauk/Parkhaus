2b54596a84899e1a4b32b3943a89a792cc1ad136
Tim <timbraukmann@gmx.de>
2026-03-03 11:13:59 +0100

    Typos korrigiert und user eingabe hinzugefügt

--- src/main.c
+++ src/main.c
@@ -6,37 +6,53 @@
 
 FUNKTION main
 {
-    //variablen Deklaration
+    // Variablen Deklaration
     Ganzzahl id = 0
     Ganzzahl zeitpunkt = 0
-    Ganzzahl  = 0
+    Ganzzahl anzahl_parkplaetze = 0
     Ganzzahl maximale_parkdauer = 0
     float simulations_dauer = 0
     Ganzzahl seed = 0
-    float wahscheinlichkeit_neues_kfz = 0
+    float wahrscheinlichkeit_neues_kfz = 0
+    Ganzzahl aendern = 0
 
-    //Parameter über Terminal eingeben
+    // Parameter ueber Terminal eingeben
     Eingabe: anzahl_parkplaetze
     Eingabe: maximale_parkdauer
-    Eingabe: simuations_dauer
+    Eingabe: simulations_dauer
     Eingabe: seed
-    Eingabe: wahscheinlichkeit_neues_kfz
+    Eingabe: wahrscheinlichkeit_neues_kfz
 
-    //zufallszahlengenerator initialisieren
-    srand(seed);
-    //initalisieren Parkhaus/simulation
-    Simulationsparameter *p_simulationsparameter = init_simulationsparameter(anzahl_parkplaetze, maximale_parkdauer, simulations_dauer, seed, wahscheinlichkeit_neues_kfz);
-    Parkhaus *p_parkhaus = init_parkhaus(p_simulationsparameter->anzahl_parkplaetze);
+    // Zufallszahlengenerator initialisieren
+    srand(seed)
 
-    // Simulationsschleife:
+    // Parkhaus und Simulationsparameter initialisieren
+    Simulationsparameter *p_simulationsparameter = init_simulationsparameter(anzahl_parkplaetze, maximale_parkdauer, simulations_dauer, seed, wahrscheinlichkeit_neues_kfz)
+    Parkhaus *p_parkhaus = init_parkhaus(p_simulationsparameter->maximale_parkdauer, p_simulationsparameter->anzahl_parkplaetze)
+
+    // Simulationsschleife
     SOLANGE zeitpunkt < simulations_dauer:
+
+        // Nutzer fragen ob Parameter geaendert werden sollen
+        Ausgabe: "Parameter aendern? (1 = ja, 0 = nein)"
+        Eingabe: aendern
+        WENN aendern == 1:
+            Eingabe: anzahl_parkplaetze
+            Eingabe: maximale_parkdauer
+            Eingabe: simulations_dauer
+            Eingabe: seed
+            Eingabe: wahrscheinlichkeit_neues_kfz
+            aktualisiere_parameter(p_parkhaus, p_simulationsparameter, anzahl_parkplaetze, maximale_parkdauer, simulations_dauer, wahrscheinlichkeit_neues_kfz, seed)
+        ENDE WENN
+
         simuliere_zeitabschnitt(p_parkhaus, p_simulationsparameter, &id, &zeitpunkt)
-        // Statisken werden eingefügt sobald sie klar sind.
-    ENDE SOLANGE 
-     // Speicher freigeben:
+        // Statistiken werden eingefuegt sobald sie klar sind.
+
+    ENDE SOLANGE
+
+    // Speicher freigeben
     free(p_parkhaus->p_parkplaetze)
     free(p_parkhaus)
     free(p_simulationsparameter)
-    GIB 0 
-
+    GIB 0
 }
