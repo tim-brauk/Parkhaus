fd25bcd2a32868e09e51678d8af7603027a733c9
tim-brauk <timbraukmann@gmx.de>
2026-03-06 22:40:28 +0100

    entferne_kfzs_maximale_parkdauer aus parkvorgang.c von pseudocode in c code überführt

--- src/parkvorgang.c
+++ src/parkvorgang.c
@@ -32,7 +32,7 @@ void fuege_kfz_hinzu(Parkhaus *p_parkhaus, Kfz *p_kfz)
 }
 
 
-entferne_kfz(Parkhaus *p_parkhaus, Kfz *p_kfz)
+void entferne_kfz(Parkhaus *p_parkhaus, Kfz *p_kfz)
 {    
     for(int i = 0; i < p_parkhaus->anzahl_parkplaetze; i++)
     {
@@ -49,19 +49,20 @@ entferne_kfz(Parkhaus *p_parkhaus, Kfz *p_kfz)
 }
 
 
-entferne_kfzs_maximale_parkdauer(Parkhaus *p_parkhaus):
+void entferne_kfzs_maximale_parkdauer(Parkhaus *p_parkhaus)
 {    
-    /*FÜR Ganzzahl i = 0, i < p_parkhaus->anzahl_parkplaetze, i++:
-     *  WENN p_parkhaus->p_parkplaetze[i].p_kfz != NULL:
-     *       WENN p_parkhaus->p_parkplaetze[i].p_kfz->verbleibende_parkdauer < 0:
-     *           p_parkhaus->p_parkplaetze[i].belegt = 0
-     *           free(p_parkhaus->p_parkplaetze[i].p_kfz)
-     *           p_parkhaus->p_parkplaetze[i].p_kfz = NULL
-     *           Ausgabe: "Ein KFZ musste aufgrund zu hoher Parkzeit entfernt werden oder aufgrund von Bauarbeiten"
-     *       ENDE WENN
-     *   ENDE WENN
-     * ENDE FÜR
-     */
-
-    //Die Funktion wird in Teil 2 des Projekts programmiert
+    for(int i = 0; i < p_parkhaus->anzahl_parkplaetze; i++)
+    {
+        if(p_parkhaus->p_parkplaetze[i].p_kfz != NULL)
+        {
+            if(p_parkhaus->p_parkplaetze[i].p_kfz->verbleibende_parkdauer < 0)
+            {
+                p_parkhaus->p_parkplaetze[i].belegt = 0;
+                free(p_parkhaus->p_parkplaetze[i].p_kfz);
+                p_parkhaus->p_parkplaetze[i].p_kfz = NULL;
+                p_parkhaus->belegte_parkplaetze--;
+                printf("Ein KFZ musste aufgrund zu hoher Parkzeit entfernt werden oder aufgrund von Bauarbeiten\n");
+            }
+        }
+    }
 }
