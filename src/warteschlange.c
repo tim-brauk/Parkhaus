cd8f47c819101bb11c9fa097d661382f6b33b340
tim-brauk <timbraukmann@gmx.de>
2026-03-06 21:47:33 +0100

    entferne_kfz_warteschlange in kfz.c von pseudocode in c-code übertragen

--- src/warteschlange.c
+++ src/warteschlange.c
@@ -23,27 +23,24 @@ void kfz_hinzufuegen_warteschlange(Parkhaus *p_parkhaus, Kfz *p_kfz)
 }
 
 
-Kfz* entferne_kfz_warteschlange(Parkhaus *p_parkhaus):
+Kfz* entferne_kfz_warteschlange(Parkhaus *p_parkhaus)
 {   
-    /*
-     * WENN p_parkhaus->p_erstes_kfz_in_der_warteschlange == NULL:
-     *     Ausgabe: "Keine kfzs in der Warteschlange"
-     *     GIB NULL
-     *     ENDE FUNKTION
-     * ENDE WENN
-     *
-     * WENN p_parkhaus->p_erstes_kfz_in_der_warteschlange->p_naechstes_kfz == NULL:
-     *     Kfz *p_kfz_zwischenspeicher = p_parkhaus->p_erstes_kfz_in_der_warteschlange
-     *     p_parkhaus->p_erstes_kfz_in_der_warteschlange = NULL
-     *     GIB p_kfz_zwischenspeicher
-     *     ENDE FUNKTION
-     * ENDE WENN
-     *
-     * Kfz *p_kfz_zwischenspeicher = p_parkhaus->p_erstes_kfz_in_der_warteschlange
-     * p_parkhaus->p_erstes_kfz_in_der_warteschlange = p_kfz_zwischenspeicher->p_naechstes_kfz
-     * p_kfz_zwischenspeicher->p_naechstes_kfz = NULL
-     * GIB p_kfz_zwischenspeicher
-     */
-
-    //Die Funktion wird in Teil 2 des Projekts programmiert
+    
+    if(p_parkhaus->p_erstes_kfz_in_der_warteschlange == NULL)
+    {
+        printf("Keine kfzs in der Warteschlange\n");
+        return NULL;
+    }
+    
+    if(p_parkhaus->p_erstes_kfz_in_der_warteschlange->p_naechstes_kfz == NULL)
+    {
+        Kfz *p_kfz_zwischenspeicher = p_parkhaus->p_erstes_kfz_in_der_warteschlange;
+        p_parkhaus->p_erstes_kfz_in_der_warteschlange = NULL;
+        return p_kfz_zwischenspeicher;
+    }
+    
+    Kfz *p_kfz_zwischenspeicher = p_parkhaus->p_erstes_kfz_in_der_warteschlange;
+    p_parkhaus->p_erstes_kfz_in_der_warteschlange = p_kfz_zwischenspeicher->p_naechstes_kfz;
+    p_kfz_zwischenspeicher->p_naechstes_kfz = NULL;
+    return p_kfz_zwischenspeicher;
 }
