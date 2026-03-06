98ef875a66c8f219767e2a9bcfa73979aa920668
tim-brauk <timbraukmann@gmx.de>
2026-03-06 22:15:52 +0100

    fuege_kfz_hinzu in warteschlange.c von psuedocode in c code überführt

--- src/parkvorgang.c
+++ src/parkvorgang.c
@@ -1,30 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "parkvorgang.h"
 
-fuege_kfz_hinzu(Parkhaus *p_parkhaus, Kfz *p_kfz):
+void fuege_kfz_hinzu(Parkhaus *p_parkhaus, Kfz *p_kfz)
 {
-  /* Ganzzahl freie_parkplaetze = (p_parkhaus->anzahl_parkplaetze) - (p_parkhaus->belegte_parkplaetze)
-   *
-   * Ganzzahl min = 0
-   * Ganzzahl max = freie_parkplaetze - 1
-   * Ganzzahl zufaelliger_parkplatz = 0
-   * zufaelliger_parkplatz = min + (rand() % (max - min + 1))
-   *
-   * FÜR Ganzzahl i = 0, i < p_parkhaus->anzahl_parkplaetze, i++:
-   *     WENN !zufaelliger_parkplatz
-   *         p_parkhaus->p_parkplaetze[i].belegt = 1
-   *         p_parkhaus->p_parkplaetze[i].p_kfz = p_kfz
-   *         p_parkhaus->belegte_parkplaetze = p_parkhaus->belegte_parkplaetze + 1
-   *         ENDE FUNKTION
-   *     ENDE WENN
-   *
-   *     WENN !p_parkhaus->p_parkplaetze[i].belegt
-   *         freie_parkplaetze = freie_parkplaetze - 1
-   *    ENDE WENN
-   * ENDE FÜR
-   */
+    int freie_parkplaetze = (p_parkhaus->anzahl_parkplaetze) - (p_parkhaus->belegte_parkplaetze);
+    int min = 0;
+    int max = freie_parkplaetze - 1;
+    int zufaelliger_parkplatz = 0;
+    zufaelliger_parkplatz = min + (rand() % (max - min + 1));
 
-   //Die Funktion wird in Teil 2 des Projekts programmiert
-
+    for(int i = 0; i < p_parkhaus->anzahl_parkplaetze; i++)
+    {
+        if(!p_parkhaus->p_parkplaetze[i].belegt)
+        {
+            if(zufaelliger_parkplatz == 0)
+            {
+                p_parkhaus->p_parkplaetze[i].belegt = 1;
+                p_parkhaus->p_parkplaetze[i].p_kfz = p_kfz;
+                p_parkhaus->belegte_parkplaetze++;
+                return;
+            }
+        zufaelliger_parkplatz--;
+        }
+    
+        if(!p_parkhaus->p_parkplaetze[i].belegt)
+        {
+            freie_parkplaetze = freie_parkplaetze - 1;
+        }
+    }
 }
 
 
