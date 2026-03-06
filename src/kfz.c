727177c097521211a7ae533c0b2343a0ad042a93
tim-brauk <timbraukmann@gmx.de>
2026-03-06 21:21:37 +0100

    kfz.c von pseudocode in c umgeschrieben

--- src/kfz.c
+++ src/kfz.c
@@ -1,19 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "kfz.h"
 
-Kfz* init_kfz(struct Parkhaus *p_parkhaus, int id, int zeitpunkt)
-{   /*
-     * Kfz *p_kfz = malloc(sizeof(Kfz))
-     * WENN p_kfz == NULL:
-     *     Ausgabe: "Malloc Fehler in init_kfz"
-     * ENDE WENN
-     * p_kfz->id = id
-     * p_kfz->einfahrtszeitpunkt = zeitpunkt
-     * Ganzzahl min = 1
-     * Ganzzahl max = p_parkhaus->maximale_parkdauer
-     * p_kfz->verbleibende_parkdauer = min + rand() % (max - min + 1)
-     * p_kfz->p_naechstes_kfz = NULL
-     * GIB p_kfz
-     */
 
-    //Die Funktion wird in Teil 2 des Projekts programmiert
+Kfz* init_kfz(struct Parkhaus *p_parkhaus, int id, int zeitpunkt)
+{   
+    Kfz *p_kfz = malloc(sizeof(Kfz));
+    if(p_kfz == NULL)
+    {       
+        printf("Malloc Fehler in init_kfz\n");
+        return NULL;
+    }
+    p_kfz->id = id;
+    p_kfz->einfahrtszeitpunkt = zeitpunkt;
+    int min = 1;
+    int max = p_parkhaus->maximale_parkdauer;
+    p_kfz->verbleibende_parkdauer = min + rand() % (max - min + 1);
+    p_kfz->p_naechstes_kfz = NULL;
+    return p_kfz;
+    
 }
