96bae144eb26f0c08369cce3f599b207f1bc0362
adamchrusciel23 <adamchrusciel2309@gmail.com>
2026-03-07 22:36:30 +0000

    erstelle Testfunktionen für entferne_kfz_nicht_vorhanden

--- tests/test_parkvorgang.c
+++ tests/test_parkvorgang.c
@@ -4,43 +4,90 @@
 #include "parkhaus.h"
 
 void test_fuege_kfz_hinzu_leer(){
-    Parkhaus *p_parkhaus = init_parkhaus(5, 40); //Erstellen eines Parkhauses mit 5 Parkplätzen und einer maximalen Parkdauer von 40 Sekunden
+    Parkhaus *p_parkhaus = init_parkhaus(5, 40); //Erstellen eines Parkhauses mit 5 Parkplätzen und einer maximalen Parkdauer von 40 
     Kfz *p_kfz = init_kfz(p_parkhaus, 1, 60); //Test Kfz erhält die ID 1 und die einfahrtzeitpunkt von 60
+    
     fuege_kfz_hinzu(p_parkhaus, p_kfz); //Fügt das Kfz zum Parkhaus hinzu
+    
     assert(p_kfz->belegte_parkplaetze == 1); //Überprüft, ob das Kfz einen Parkplatz zugewiesen bekommen hat
+    
     for(int i = 0; i < p_parkhaus->anzahl_parkplaetze; i++){
         if(p_parkhaus->parkplaetze[i] != NULL){
             assert(p_parkhaus->parkplaetze[i]->p_kfz == p_kfz); //Überprüft, ob das Kfz auch im Parkhaus angekommen ist
         }
     }
+    
     free(p_kfz);
     free(p_parkhaus);
 }
 
 void test_fuege_kfz_hinzu_belegt(){
-    Parkhaus *p_parkhaus = init_parkhaus(5, 40); //Erstellen eines Parkhauses mit 5 Parkplätzen und einer maximalen Parkdauer von 40 Sekunden
+    Parkhaus *p_parkhaus = init_parkhaus(5, 40); //Erstellen eines Parkhauses mit 5 Parkplätzen und einer maximalen Parkdauer von 40
     Kfz *p_kfz1 = init_kfz(p_parkhaus, 3, 60); //Test Kfz erhält die ID 3 und die einfahrtzeitpunkt von 60
-    Parkhaus->parkplaetze[0] = init_kfz(p_parkhaus, 1, 60);
-    Parkhaus->parkplaetze[0]->belegt = 1; //Belegt den ersten Parkplatz
-    Parkhaus->parkplaetze[1] = init_kfz(p_parkhaus, 2, 60);
-    Parkhaus->parkplaetze[1]->belegt = 1; //Belegt den zweiten Parkplatz
+    
+    p_parkhaus->parkplaetze[0] = init_kfz(p_parkhaus, 1, 60);
+    p_parkhaus->parkplaetze[0]->belegt = 1; //Belegt den ersten Parkplatz
+    p_parkhaus->parkplaetze[1] = init_kfz(p_parkhaus, 2, 60);
+    p_parkhaus->parkplaetze[1]->belegt = 1; //Belegt den zweiten Parkplatz
+    
     fuege_kfz_hinzu(p_parkhaus, p_kfz1); //Fügt das Kfz zum Parkhaus hinzu
+    
     for(int i = 0; i < 2; i++){
         if(p_parkhaus->parkplaetze[i] != NULL){
             assert(p_parkhaus->parkplaetze[i]->p_kfz != p_kfz1); //Überprüft, ob das Kfz nicht auf einem belegten Parkplatz gelandet ist
         }
     }
+    
     for(int i = 2; i < p_parkhaus->anzahl_parkplaetze; i++){
         if(p_parkhaus->parkplaetze[i] != NULL){
             assert(p_parkhaus->parkplaetze[i]->p_kfz == p_kfz1); //Überprüft, ob das Kfz auf einem freien Parkplatz gelandet ist
         }
     }
+    
     free(p_kfz1);
     free(p_parkhaus);
 }
 
-int main(){
-    test_fuege_kfz_hinzu_leer();
-    test_fuege_kfz_hinzu_belegt();
-    return 0;
+
+void test_entferne_kfz_normalfall(){
+    Parkhaus *p_parkhaus = init_parkhaus(5, 40); //Erstellen eines Parkhauses mit 5 Parkplätzen und einer maximalen Parkdauer von 40 
+    Kfz *p_kfz = init_kfz(p_parkhaus, 1, 60); //Test Kfz erhält die ID 1 und die einfahrtzeitpunkt von 60
+
+    p_parkhaus->p_parkplaetze[0] = init_kfz(p_parkhaus, 2, 60);
+    p_parkhaus->p_parkplaetze[0]->belegt = 1;
+    p_parkhaus->p_parkplaetze[1] = init_kfz(p_parkhaus, 3, 60);
+    p_parkhaus->p_parkplaetze[1]->belegt = 1;
+    p_parkhaus->p_parkplaetze[2] = p_kfz;
+    p_parkhaus->p_parkplaetze[2]->belegt = 1;
+    p_parkhaus->belegte_parkplaetze = 3;
+    //Volles Parkhaus davon 2 Plätze belegt und 1 Platz mit dem Test Kfz belegt
+    
+    entferne_kfz(p_parkhaus, p_kfz); //Entfernt das Kfz aus dem Parkhaus
+    
+    assert(p_parkhaus->p_parkplaetze[2] == NULL); //Überprüft, ob der Parkplatz des Kfz jetzt frei ist
+    assert(p_parkhaus->belegte_parkplaetze == 2); //Überprüft, ob die Anzahl der belegten Parkplätze um 1 reduziert wurde
+
+    free(p_kfz);
+    free(p_parkhaus);
+}
+
+void test_entferne_kfz_nicht_vorhanden(){
+    Parkhaus *p_parkhaus = init_parkhaus(5, 40); //Erstellen eines Parkhauses mit 5 Parkplätzen und einer maximalen Parkdauer von 40 
+    Kfz *p_kfz = init_kfz(p_parkhaus, 1, 60); //Test Kfz erhält die ID 1 und die einfahrtzeitpunkt von 60
+
+    p_parkhaus->p_parkplaetze[0] = init_kfz(p_parkhaus, 2, 60);
+    p_parkhaus->p_parkplaetze[0]->belegt = 1;
+    p_parkhaus->p_parkplaetze[1] = init_kfz(p_parkhaus, 3, 60);
+    p_parkhaus->p_parkplaetze[1]->belegt = 1;
+    p_parkhaus->belegte_parkplaetze = 2;
+    //Parkhaus mit 2 belegten Parkplätzen aber das Test Kfz ist nicht im Parkhaus
+
+    entferne_kfz(p_parkhaus, p_kfz); //Versucht das Kfz zu entfernen, obwohl es nicht im Parkhaus ist
+    
+    assert(p_parkhaus->p_parkplaetze[0] != NULL); //Überprüft, ob der erste Parkplatz weiterhin belegt ist
+    assert(p_parkhaus->p_parkplaetze[1] != NULL); //Überprüft, ob der zweite Parkplatz weiterhin belegt ist
+    assert(p_parkhaus->belegte_parkplaetze == 2); //Überprüft, ob die Anzahl der belegten Parkplätze unverändert geblieben ist
+
+    free(p_kfz);
+    free(p_parkhaus);
 }
