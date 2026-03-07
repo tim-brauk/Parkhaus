cd859397f6b2d3af2f34143c45483f59d5d0b9ba
adamchrusciel23 <adamchrusciel2309@gmail.com>
2026-03-07 21:27:41 +0000

    erstelle Testfunktionen für init_kfz

---
+++ tests/test_kfz.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include "parkhaus.h"
+#include <stdlib.h>
+#include "kfz.h"
+
+void test_init_kfz_id(){
+    Parkhaus *p_parkhaus = init_parkhaus(NULL, 0, 40); //Erstellen eines Parkhauses mit 0 Parkplätzen, aber einer maximalen Parkdauer von 40 Sekunden
+    Kfz *p_kfz = init_kfz(p_parkhaus, 1, 60); //Test Kfz erhält die ID 1 und die einfahrtzeitpunkt von 60
+    
+    assert(p_kfz->id == 1); //Überprüfen, ob die ID korrekt gesetzt wird
+    
+    free(p_kfz);
+    free(p_parkhaus);
+}
+
+void test_init_kfz_parkdauer(){
+    Parkhaus *p_parkhaus = init_parkhaus(NULL, 0, 40); //Erstellen eines Parkhauses mit 0 Parkplätzen, aber einer maximalen Parkdauer von 40 Sekunden
+    Kfz *p_kfz = init_kfz(p_parkhaus, 1, 60); //Test Kfz erhält die ID 1 und die einfahrtzeitpunkt von 60
+    
+    assert(p_kfz->parkdauer >= 1 && p_kfz->parkdauer <= 40); //Überprüfen, ob die Parkdauer innerhalb des erwarteten Bereichs liegt
+    
+    free(p_kfz);
+    free(p_parkhaus);
+}
+
+int main() {
+    test_init_kfz_id();
+    test_init_kfz_parkdauer();
+    return 0;
+}
