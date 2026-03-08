5a75c599982b836f5ca5eb1148dcd9c97e35f1cb
adamchrusciel23 <adamchrusciel2309@gmail.com>
2026-03-08 01:01:54 +0000

    erstelle Testfunktionen für entferne_kfz_warteschlange

--- tests/test_warteschlange.c
+++ tests/test_warteschlange.c
@@ -28,3 +28,34 @@ void test_kfz_hinzufuegen_warteschlange_normal(){
     free(p_parkhaus);
     free(p_kfz);
 }
+
+void test_entferne_kfz_warteschlange_normal(){
+    Parkhaus *p_parkhaus = init_parkhaus(0, 40);
+    Kfz *p_kfz1 = init_kfz(p_parkhaus, 1, 60);
+    Kfz *p_kfz2 = init_kfz(p_parkhaus, 2, 61);
+    Kfz *p_kfz3 = init_kfz(p_parkhaus, 3, 62);
+
+    p_parkhaus->p_erster_in_der_warteschlange = p_kfz1;
+    p_parkhaus->p_erster_in_der_warteschlange->p_naechster = p_kfz2;
+    p_parkhaus->p_erster_in_der_warteschlange->p_naechster->p_naechster = p_kfz3;
+
+    entferne_kfz_warteschlange(p_parkhaus);
+    assert(p_parkhaus->p_erster_in_der_warteschlange == p_kfz2);
+    assert(p_parkhaus->p_erster_in_der_warteschlange->p_naechster == p_kfz3);
+    assert(p_kfz1->p_naechster == NULL);
+
+    free(p_parkhaus);
+    free(p_kfz1);
+    free(p_kfz2);
+    free(p_kfz3);
+}
+
+void test_entferne_kfz_warteschlange_leer(){
+    Parkhaus *p_parkhaus = init_parkhaus(0, 40);
+    
+    entferne_kfz_warteschlange(p_parkhaus);
+    
+    assert(p_parkhaus->p_erster_in_der_warteschlange == NULL);
+    
+    free(p_parkhaus);
+}
