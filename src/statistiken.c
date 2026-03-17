53b359c772a0c9475613ea71b5d3f98fab5704d8
lukas-meissner <lukas174m@icloud.com>
2026-03-17 12:08:43 +0100

    Fehler im Programm behoben

--- src/statistiken.c
+++ src/statistiken.c
@@ -213,5 +213,5 @@ void ausgabe_statistiken(SimulationsStats *p_statistik)
     printf("\nDurchschnittliche länge der Warteschlange liegt bei: %.2f", berechne_durchschnitt_warteschlangenlaenge( p_statistik));
     printf("\nDurchschnittliche Wartezeit liegt bei: %.2f", berechne_durchschnittliche_wartezeit( p_statistik));
     printf("\nMaximale Auslastung: %.2f%%", p_statistik->maximale_auslastung);
-    printf("\nMaximale Länge der Warteschlange: %d", p_statistik->maximale_warteschlangenlaenge);
+    printf("\nMaximale Länge der Warteschlange: %d\n", p_statistik->maximale_warteschlangenlaenge);
 }
