fbf1a17ca980d9170e16fd218f153f5b5b51fc83
adamchrusciel23 <adamchrusciel2309@gmail.com>
2026-03-18 19:46:43 +0000

    korrigiere kfz.h

--- src/kfz.c
+++ src/kfz.c
@@ -20,7 +20,7 @@ Kfz* init_kfz(struct Parkhaus *p_parkhaus, int *p_id, int zeitpunkt)
         printf("Malloc Fehler in init_kfz\n");
         return NULL;
     }
-    p_kfz->p_id = *p_id;
+    p_kfz->id = *p_id;
     (*p_id)++;
     p_kfz->einfahrtszeitpunkt = zeitpunkt;
     int min = 1;
